da077fc31f44a8d3f6cf519c93bec395f39b984b
Hippolyte Larzul <hippolarzul@gmail.com>
2022-12-13 09:27:54 +0100

    adaptive theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Space Pic',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
