a2e24bce346a50a26eeef08588ce36652a10d6c1
Hippolyte Larzul <hippolarzul@gmail.com>
2022-12-27 13:31:24 +0100

    add translation

--- lib/views/detail_widget.dart
+++ lib/views/detail_widget.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: no_logic_in_create_state
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
@@ -5,9 +8,37 @@ import 'package:vimeo_video_player/vimeo_video_player.dart';
 
 import '../models/space_data.dart';
 
-class DetailWidget extends StatelessWidget {
+class DetailWidget extends StatefulWidget {
   final SpaceData spaceData;
   const DetailWidget({super.key, required this.spaceData});
+
+  @override
+  DetailWidgetState createState() => DetailWidgetState(spaceData: spaceData);
+}
+
+class DetailWidgetState extends State<DetailWidget> {
+  final SpaceData spaceData;
+  DetailWidgetState({required this.spaceData});
+
+  bool translated = false;
+  String _explanationTranslated = '';
+
+  _fetchTranslateDescription() async {
+    var dateToTranslate = spaceData.date.substring(2, 10).replaceAll('-', '');
+    var response = await Dio().get("https://www.cidehom.com/apod.php",
+        queryParameters: {"_date": dateToTranslate});
+
+    setState(() {
+      _explanationTranslated = response.data.substring(
+          response.data.indexOf('<p>') + 3, response.data.indexOf('</p>'));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchTranslateDescription();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,11 +111,33 @@ class DetailWidget extends StatelessWidget {
               ),
               const Text("Description : ",
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
-              Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Text(spaceData.explanation,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.normal, fontSize: 20))),
+              if (translated)
+                Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Text(_explanationTranslated.toString(),
+                        style: const TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 20)))
+              else
+                Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Text(spaceData.explanation,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 20))),
+              ElevatedButton.icon(
+                  onPressed: () {
+                    setState(() {
+                      translated = !translated;
+                    });
+                  },
+                  icon: const Icon(Icons.translate),
+                  label: const Text('Translate')),
+              const Padding(padding: EdgeInsets.all(20)),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Back to the list"),
+              ),
             ],
           ),
         ));
